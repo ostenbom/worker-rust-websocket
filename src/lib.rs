402eca5dcce5b74de1cfff52f673dd0004d34a69
Oliver Stenbom <oliver@stenbom.eu>
2023-06-12 09:29:06 +0200

    Add websocket connect example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use worker::*;
 
+use futures::{stream::StreamExt};
+
 mod utils;
 
 fn log_request(req: &Request) {
@@ -19,7 +21,24 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // Optionally, get more helpful error messages written to the console in the case of a panic.
     utils::set_panic_hook();
 
-    console_log!("worker headers: {:?}", req.headers());
+    let ws = WebSocket::connect("wss://socketsbay.com/wss/v2/1/demo/".parse()?).await?;
 
-    Response::ok("Hello from Workers!")
+    // It's important that we call this before we send our first message, otherwise we will
+    // not have any event listeners on the socket to receive the echoed message.
+    let mut event_stream = ws.events()?;
+
+    ws.accept()?;
+    ws.send_with_str("Hello, world!")?;
+
+    while let Some(event) = event_stream.next().await {
+        let event = event?;
+
+        if let WebsocketEvent::Message(msg) = event {
+            if let Some(text) = msg.text() {
+                return Response::ok(text);
+            }
+        }
+    }
+
+    Response::error("never got a message echoed back :(", 500)
 }
