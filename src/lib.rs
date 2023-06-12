fb99ca0d065354be826a5fe2ac337b02cdf56fbb
Oliver Stenbom <oliver@stenbom.eu>
2023-06-12 14:55:24 +0200

    Print all headers in logs

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,10 @@ mod utils;
 
 fn log_request(req: &Request) {
     console_log!(
-        "{} - [{}], located at: {:?}, within: {}",
+        "{} - [{}], headers: {:?}",
         Date::now().to_string(),
         req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or_else(|| "unknown region".into())
+        req.headers(),
     );
 }
 
